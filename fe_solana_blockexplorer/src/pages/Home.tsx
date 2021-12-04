24d0c61e93fe94bee56164caa799837d6dae788e
Zoran Terhes <zterhes1@gmail.com>
2021-12-04 18:07:28 +0100

    implement home page

--- fe_solana_blockexplorer/src/pages/Home.tsx
+++ fe_solana_blockexplorer/src/pages/Home.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import {
+    Grid,
+    Typography,
+    ButtonGroup,
+    IconButton,
+    TextField,
+    Box,
+    Button
+} from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 
-export default ()=>{
-    return(
-        <div>Hello</div>
+
+export default () => {
+    const [input, setInput] = useState<string>();
+    const route = `/searchVallet:${input}`
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value)
+    }
+
+    const handleClick = () => {
+
+    }
+
+    return (
+        <Grid container direction="column" justifyContent="center" alignItems="center" minHeight="100vh">
+            <Grid>
+                <Typography mt={2} variant="h1">Solana BlockExplorer</Typography>
+            </Grid>
+            <Box display="flex" justifyContent="center" alignItems="center">
+                {/* <TextField margin="normal" id="wallet_search" label="Find a wallet" variant="outlined"></TextField> */}
+                <TextField
+                    margin="normal"
+                    id="wallet_search"
+                    label="Search for a wallet"
+                    variant="outlined"
+                    onChange={handleChange} />
+                <IconButton sx={{ height: "100%" }} href={route}><SearchIcon /></IconButton>
+            </Box>
+            <Grid>
+                <ButtonGroup variant="outlined" size="large">
+                    <Button href="/tokens">List Tokens</Button>
+                    <Button href="/blocks">Last Blocks</Button>
+                </ButtonGroup>
+            </Grid>
+        </Grid>
+
     )
 }
--- fe_solana_blockexplorer/src/styles/theme.ts
+++ fe_solana_blockexplorer/src/styles/theme.ts
@@ -1,9 +1,10 @@
-import {createTheme} from '@mui/material/styles'
-import {red} from '@mui/material/colors'
-import { dark } from '@mui/material/styles/createPalette'
+import { createTheme } from '@mui/material/styles'
 
 export default createTheme({
-    palette:{
-       mode:'dark',
+    palette: {
+        mode: 'dark',
+        background: {
+            default: '#001e3c',
+        }
     }
 })
