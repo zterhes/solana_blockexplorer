0ce9216334f37ee667e79858320b1a8c11f46c72
Zoran Terhes <zterhes1@gmail.com>
2021-12-04 13:03:37 +0100

    init styles

---
+++ fe_solana_blockexplorer/src/styles/theme.ts
@@ -0,0 +1,9 @@
+import {createTheme} from '@mui/material/styles'
+import {red} from '@mui/material/colors'
+import { dark } from '@mui/material/styles/createPalette'
+
+export default createTheme({
+    palette:{
+       mode:'dark',
+    }
+})
